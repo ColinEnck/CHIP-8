31efc1418dfaddf9f3f4bb2d3319f12bebb1bdda
ColinEnck <colinenck@gmail.com>
2023-08-16 11:24:56 -0400

    automatic commit

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stack>
 #define LENGTH 64
 #define WIDTH 32
 
 void* check_malloc(unsigned int size);
 void usage();
-char* fetch(int &PC, uint16_t mem[]);
 
 int main(int argc, char** argv)
 {
@@ -20,14 +20,15 @@ int main(int argc, char** argv)
     return 0;
   }
   // setting up the environment
-  uint16_t* mem = (uint16_t*) check_malloc(4096-512); // emulated memory minus free space at beginning
-  uint16_t stack[16]; // 2-byte wide stack
+  char* mem = (char*) check_malloc(4096-512); // emulated memory minus free space at beginning
+  std::stack<uint16_t> stack; // 16-bit sized stack
   int PC; // program counter
   int I; // index register
-  char SP; // stack pointer; 1 byte long
+  int8_t SP; // stack pointer; 1 byte long
   int delay; // delay timer
   int sound; // sound timer
-  char registers[16]; // one-byte register  
+  int8_t registers[16]; // one-byte registers;
+  char instruction[4]; // stores the instruction as a string
 
   // reading from file
   FILE *fptr;
@@ -46,26 +47,75 @@ int main(int argc, char** argv)
   fread(mem, 1, size, fptr);
   fclose(fptr);
 
-  for (int i = 0; i < size/2; ++i)
-    printf("%04X\n", mem[i]);
-
   sf::RenderWindow window(sf::VideoMode(LENGTH * 20, WIDTH * 20), "Screen");
+
   bool screen[32][64];
   for (int i = 0; i < 32; ++i)
     for (int j = 0; j < 64; ++j)
       screen[i][j] = false;
 
+  sf::RectangleShape drawer(sf::Vector2f(20.f, 20.f));
+
+  bool keys[16];
+   
   while (window.isOpen())
   {
+    for (int i = 0; i < 16; ++i)
+      keys[i] = false;
+
+    // handle user input here
     sf::Event event;
     while (window.pollEvent(event))
     {
       // key-press logic goes here
       if (event.type == sf::Event::Closed)
         window.close();
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        window.close();
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num0))
+        keys[0] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1))
+        keys[1] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
+        keys[2] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num3))
+        keys[3] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num4))
+        keys[4] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num5))
+        keys[5] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num6))
+        keys[6] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num7))
+        keys[7] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num8))
+        keys[8] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num9))
+        keys[9] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        keys[10] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::B))
+        keys[11] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+        keys[12] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        keys[13] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+        keys[14] = true;
+      else if (sf::Keyboard::isKeyPressed(sf::Keyboard::F))
+        keys[15] = true;
     }
-
+    
+    // draw stuff
     window.clear();
+    for (int i = 0; i < 32; ++i)
+      for (int j = 0; j < 64; ++j)
+        if (screen[i][j])
+        {
+          drawer.setPosition(j*20, i*20);
+          window.draw(drawer);
+        }
+    
     window.display();
   }
 
@@ -89,13 +139,4 @@ void usage()
   printf("Please specify the path of the chip-8 file as a command line argument.\n");
   printf("USAGE:\n");
   printf("./chip8 [FILENAME]\n");
-}
-
-char* fetch(int &PC, uint16_t mem[])
-{
-  char* instruction = (char*) check_malloc(4);
-  int num = mem[(PC - 512)/2]; // offset for blank memory and uint16_t length
-  sprintf(instruction, "%04X", num);
-  PC += 2;
-  return instruction;
 }
